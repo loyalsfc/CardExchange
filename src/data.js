9b0643861cae1bd9776b42704544d957bb3f821c
Olumide Bambe <loyalsfc@gmail.com>
2022-10-06 06:17:11 +0100

    Adding rate page functionalities

--- src/data.js
+++ src/data.js
@@ -1,19 +1,70 @@
 export default [
-    {steam: [{20: 9600}, {100: 50300}]},
-    {google: [ {50: 21000}, {100: 42100}]},
-    {box: [{50:20000}, {100:40200}]},    
-    {Amazon: [{50: 21600}, {100: 44000}]},
-    {Sephora: [{50: 21600}, {100: 47000}]},
-    {Messi: [{100: 36000}, {500: 182000}]},
-    {eBay: [{50: 18800}, {100: 41500}]},
-    {ND: [{100: 46000}, {500: 230000}]},   
-    {Apple: [{50: 23400}, {100: 52000}]},  
-    {NiKe: [{100: 44500}, {500: 223}]},
-    {Vanilla: [{100: 37300}]},
-    {Amex: [{100: 40200}, {500:225000}]},
-    {WalmartVisa: [{"$100-$500": 530}]},
-    {masterVisa: [{100: 40900},  {500: 241000}]},
-    {zelle: [{"$100 Above": 470}]}, 
-    {PayPal: [{"$50 Above": 470}]},
-    {cashapp: [{"$20--$5k": 480}]}  
+    {
+        steam: [{20: 9600}, {100: 50300}], 
+        enterValue: false
+    },
+    {
+        google: [ {50: 21000}, {100: 42100}], 
+        enterValue: false
+    },
+    {
+        box: [{50:20000}, {100:40200}], 
+        enterValue: false
+    },    
+    {
+        Amazon: [{50: 21600}, {100: 44000}], 
+        enterValue: false
+    },
+    {
+        Sephora: [{50: 21600}, {100: 47000}], 
+        enterValue: false
+    },
+    {
+        Messi: [{100: 36000}, {500: 182000}], 
+        enterValue: false
+    },
+    {
+        eBay: [{50: 18800}, {100: 41500}], 
+        enterValue: false
+    },
+    {
+        ND: [{100: 46000}, {500: 230000}], 
+        enterValue: false
+    },   
+    {
+        Apple: [{50: 23400}, {100: 52000}], 
+        enterValue: false
+    },  
+    {
+        NiKe: [{100: 44500}, {500: 223}], 
+        enterValue: false
+    },
+    {
+        Vanilla: [{100: 37300}], 
+        enterValue: false
+    },
+    {
+        Amex: [{100: 40200}, {500:225000}], 
+        enterValue: false
+    },
+    {
+        WalmartVisa: [{"100-$500": 530}], 
+        enterValue: true
+    },
+    {
+        masterVisa: [{100: 40900},  {500: 241000}], 
+        enterValue: false
+    },
+    {
+        zelle: [{"100 Above": 470}], 
+        enterValue: true
+    }, 
+    {
+        PayPal: [{"50 Above": 470}], 
+        enterValue: true
+    },
+    {
+        cashapp: [{"20--$5k": 480}], 
+        enterValue: true
+    }  
 ]
--- src/pages/Rates.jsx
+++ src/pages/Rates.jsx
@@ -1,13 +1,51 @@
+import { useState, useEffect, useRef } from "react";
+
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Cards from "../data"
 
 function Rates(){
+    const [cardCategory, setCardCategory] = useState(Cards[0].steam);
+    const [giftCard, setGiftCard] = useState("Steam");
+    const [cardValue, setCardValue] = useState('default');
+    const [cardRate, setCardRate] = useState(0);
+    const [cardNaira, setCardNaira] = useState(0);
+    const [inputMode, setInputMode] = useState(false);
+    const qtyInput = useRef()
 
-    const cardsCategory = Cards.map((item, index) => {
-        return <option key={index}>{Object.keys(item)}</option>
+    const handleChange = (event) => {        
+        setCardRate(0)
+        const index = event.target.value
+        for (const key in Cards[index]) {
+            if (Object.hasOwnProperty.call(Cards[index], key)) {
+                setInputMode(Cards[index][key]);
+                if(key == "enterValue") return
+                const element = Cards[index][key];
+                setCardCategory(element)
+                setGiftCard(key)
+            }
+        }
+        setCardRate(0)
+    }
+
+    useEffect(() => {
+        setCardNaira(cardRate * 1)
+    }, [cardRate])
+
+    const setRate = (event) => {
+        setCardValue(event.target.value)
+        setCardRate(event.target.value)
+    }
+
+    const cardCategories = cardCategory.map((item, index) => {
+        return <option value={item[Object.keys(item)]} key={index}>{giftCard} - ${Object.keys(item)}</option>
     })
-    console.log(Object.keys(Cards[0]))
+
+    const giftCards = Cards.map((item, index) => {
+        let nameofCard = Object.keys(item);
+        nameofCard.pop();
+        return <option value={index} key={index}>{nameofCard}</option>
+    })
 
     return(
         <>
@@ -16,12 +54,23 @@ function Rates(){
                 <div className="container mx-auto ">
                     <h2 className="section-title">Rate Calculator</h2>
                     <p className="section-subtitle text-white mb-10">Check the current rate of different gift cards in Naira using our gift card rate calculator.</p>
-                    <div className="bg-white w-3/4 rounded-lg px-3 py-4 mx-auto">
-                        <select className="w-full focus:outline-0 bg-transparent">
-                            <option>Select Gift Card</option>
-                            {cardsCategory}
+                    <div className="bg-white w-3/4 rounded-lg px-3 py-4 mx-auto mb-4">
+                        <select className="w-full focus:outline-0 bg-transparent" onChange={handleChange}>
+                            <option disabled>Select Gift Card</option>
+                            {giftCards}
                         </select>
                     </div>
+                    <div className="bg-white w-3/4 rounded-lg px-3 py-4 mx-auto mb-4">
+                        <select className="w-full focus:outline-0 bg-transparent" onChange={setRate} value={cardValue}>
+                            <option value='default'>Select Card Category</option>
+                            {cardCategories}
+                        </select>
+                    </div>
+                    <div className="bg-white w-3/4 rounded-lg px-3 py-4 mx-auto mb-4">
+                        <input type="number" ref={qtyInput} disabled className="w-full focus:outline-0 bg-transparent disabled:bg-[#eee]" />
+                    </div>
+                    <h3 className="text-center text-white mt-8">Cash in Naira</h3>
+                    <p className="text-6xl text-center text-white font-bold">₦{cardNaira}</p>
                 </div>
             </main>
                 
